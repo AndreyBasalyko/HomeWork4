fa2b719538059547fa04ac625588a72a115d33c8
Basalyko Andrey <basalykoa.s@gmail.com>
2022-07-11 20:35:18 +0300

    Выполнил третью задачу

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿double[,] a = new double[1, 8];
+ 
+Random random = new Random();
+for ( int i = 0; i < 1; i++)
+{
+    Console.Write("[");
+    for (int j = 0; j < 8; j++)
+    {   
+     
+        a[i, j] = random.Next(100);
+        Console.Write( "{0,4}", a[i, j] );
+    } 
+    Console.Write("]");
+}
