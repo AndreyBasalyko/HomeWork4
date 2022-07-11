f801949f078791f5413d6c788e2b926222d2fccf
Basalyko Andrey <basalykoa.s@gmail.com>
2022-07-11 19:39:32 +0300

    Выполнил вторую задачу

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число...");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+void Method(int n)
+{
+    while (n > 0)
+{
+    sum = sum + n % 10;
+    n = n / 10;
+}
+Console.Clear();
+Console.Write(num);
+Console.Write(" -->");
+Console.Write(sum);
+} 
+Method(num);
